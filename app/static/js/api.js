5b0015b54715e497d33e8b1e71aab38e785edb68
purewave0 <purewave_@outlook.com>
2025-07-07 13:31:42 -0300

    feat(main): fetch readings in the Readings page

--- app/static/js/api.js
+++ app/static/js/api.js
@@ -2,4 +2,8 @@ const Api = {
     fetchSensors() {
         return fetch('/api/sensors');
     },
+
+    fetchLatestSensorReadings() {
+        return fetch('/api/readings/latest');
+    },
 };
---
+++ app/static/js/readings.js
@@ -0,0 +1,47 @@
+function createSensorReadingsCard(sensor, readings) {
+    const card = document.createElement('div');
+    card.dataset.sensorId = sensor.id;
+    card.className = 'sensor-readings';
+    card.innerHTML = `
+        <h2 class="sensor-name"></h2>
+        <ol class="readings"></ol>
+    `;
+
+    const sensorName = card.querySelector('.sensor-name');
+    sensorName.textContent = sensor.name;
+
+    // TODO: use a graph instead. this is just a prototype
+    const readingsDestination = card.querySelector('.readings');
+    const readingsFragment = new DocumentFragment();
+    for (const reading of readings) {
+        const readingItem = document.createElement('li');
+        readingItem.textContent =
+            `${reading.temperature} °C at ${reading.created_on}`;
+        readingsFragment.append(readingItem);
+    }
+    readingsDestination.append(readingsFragment);
+
+    return card;
+}
+
+
+document.addEventListener('DOMContentLoaded', async () => {
+    const sensorReadingsDestination = document.getElementById('sensor-readings');
+
+    const response = await Api.fetchSensors();
+    const sensors = await response.json();
+
+    Api.fetchLatestSensorReadings()
+        .then((response) => response.json())
+        .then((sensorReadings) => {
+            const fragment = new DocumentFragment();
+
+            for (const sensor of sensors) {
+                const readings = sensorReadings[sensor.id];
+                const card = createSensorReadingsCard(sensor, readings);
+                fragment.append(card);
+            }
+
+            sensorReadingsDestination.append(fragment);
+        });
+});
