75eda4a4851a7ba784d7f0655c0e0725e6c23720
purewave0 <purewave_@outlook.com>
2025-07-08 20:11:22 -0300

    feat(reports): fetch sensors

--- app/static/js/reports.js
+++ app/static/js/reports.js
@@ -1,4 +1,18 @@
 document.addEventListener('DOMContentLoaded', () => {
+    const sensorsSelect = document.getElementById('sensor');
+    // TODO: loading
+    Api.fetchSensors()
+        .then((response) => response.json())
+        .then((sensors) => {
+            for (const sensor of sensors) {
+                const option = document.createElement('option');
+                option.value = sensor.id;
+                option.textContent = sensor.name;
+                sensorsSelect.append(option);
+            }
+            sensorsSelect.disabled = false;
+        });
+
     const notes = document.getElementById('notes');
     const notesLengthCount = document.getElementById('notes-current-length');
     notes.addEventListener('input', () => {
