392c930dafc074daf24289dc48197317d46b2970
purewave0 <purewave_@outlook.com>
2025-07-08 21:10:33 -0300

    feat(reports): require datetimes to be filled when Custom is selected

--- app/static/js/reports.js
+++ app/static/js/reports.js
@@ -13,6 +13,33 @@ document.addEventListener('DOMContentLoaded', () => {
             sensorsSelect.disabled = false;
         });
 
+    const datetimeRadios = {
+        'start': {
+            'today': document.getElementById('start-today'),
+            'custom': document.getElementById('start-custom'),
+        },
+        'end': {
+            'today': document.getElementById('end-today'),
+            'custom': document.getElementById('end-custom'),
+        },
+    };
+    const customDatetimeInputs = {
+        'start': document.getElementById('start-custom-value'),
+        'end': document.getElementById('end-custom-value'),
+    }
+
+    // when 'custom' is marked, require the relevant datetime input to be filled
+    for (const radio of Object.values(datetimeRadios.start)) {
+        radio.addEventListener('change', () => {
+            customDatetimeInputs.start.required = datetimeRadios.start.custom.checked;
+        });
+    }
+    for (const radio of Object.values(datetimeRadios.end)) {
+        radio.addEventListener('change', () => {
+            customDatetimeInputs.end.required = datetimeRadios.end.custom.checked;
+        });
+    }
+
     const notes = document.getElementById('notes');
     const notesLengthCount = document.getElementById('notes-current-length');
     notes.addEventListener('input', () => {
