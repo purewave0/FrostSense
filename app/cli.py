724a3502eb1ba726e764a7ffe095a571a630126c
purewave0 <purewave_@outlook.com>
2025-07-15 19:37:43 -0300

    feat(cli): add --continuous flag to seed-readings

--- app/cli.py
+++ app/cli.py
@@ -2,10 +2,11 @@
 
 import datetime as dt
 from random import randrange
+from time import sleep
 
 import click
 
-from app.dbapi import create_sensor, get_sensors, create_readings
+from app.dbapi import create_sensor, get_sensors, create_reading, create_readings
 
 
 def register_commands(app):
@@ -41,14 +42,39 @@ def register_commands(app):
         show_default=True,
         help='Seconds of interval between the readings'
     )
-    def seed_readings(count, interval):
+    @click.option(
+        '--continuous',
+        is_flag=True,
+        default=False,
+        show_default=True,
+        help='Send readings continuously, one by one, instead of all at once'
+        # TODO: configurable interval for --continuous
+    )
+    def seed_readings(count, interval, continuous):
         """Seed the database sensors with `count` random readings.
 
         Starting from `interval` * `count` seconds before now, readings will have
         `interval` seconds between each other.
+
+        If --continuous, then readings will be sent one by one instead.
         """
         all_sensors = get_sensors()
         start_time = dt.datetime.utcnow() - dt.timedelta(seconds=interval*count)
+
+        def get_random_temperature():
+            return randrange(-25000, 25000)/1000
+
+        if continuous:
+            while True:
+                for sensor in all_sensors:
+                    temperature = get_random_temperature()
+                    click.echo(
+                        f'seeding reading={temperature}°C to sensor_id={sensor["id"]}'
+                    )
+                    create_reading(sensor['id'], temperature)
+                click.echo('waiting 2s...\n')
+                sleep(2)
+            return
 
         for sensor in all_sensors:
             readings = []
@@ -56,7 +82,7 @@ def register_commands(app):
                 readings.append(
                     {
                         'sensor_id': sensor['id'],
-                        'temperature': randrange(-25000, 25000)/1000,
+                        'temperature': get_random_temperature(),
                         'created_on': start_time + dt.timedelta(seconds=interval*times),
                     }
                 )
