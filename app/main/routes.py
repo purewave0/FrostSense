c7352d8e871e4fce981eeca888da08d6743dbd78
purewave0 <purewave_@outlook.com>
2025-07-19 14:42:36 -0300

    feat(main): display reports by token

--- app/main/routes.py
+++ app/main/routes.py
@@ -1,6 +1,7 @@
-from flask import render_template, redirect
+from flask import render_template, redirect, abort
 
 from app.main import bp
+from app.api.report import get_report_file
 
 
 @bp.route('/')
@@ -22,3 +23,12 @@ def sensors():
 @bp.route('/reports')
 def reports():
     return render_template('main/sections/reports.html')
+
+@bp.route('/reports/<token>')
+def get_report(token):
+    try:
+        report_html = get_report_file(token)
+    except IOError:
+        return abort(404)
+
+    return report_html
