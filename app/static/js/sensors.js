6df4921a700d6266247a6222770a8b1449a7e9be
purewave0 <purewave_@outlook.com>
2025-08-12 21:28:09 -0300

    feat(main): add basic design to Sensors

--- app/static/js/sensors.js
+++ app/static/js/sensors.js
@@ -1,19 +1,28 @@
-function createSensorCard(sensor, todayReadingsCount) {
+function createSensorCard(sensor, todayReadingsCount, locales) {
     const card = document.createElement('div');
     card.className = 'sensor';
     card.innerHTML = `
         <h2 class="sensor-name"></h2>
-        <p class="sensor-info"></p>
+        <ul class="sensor-info">
+            <li>
+                Created on: <span class="created-on"></span>
+            </li>
+            <li>
+                Readings today: <span class="readings-today"></span>
+            </li>
+        </ul>
     `;
 
     const sensorName = card.querySelector('.sensor-name');
     sensorName.textContent = sensor.name;
+    sensorName.title = sensor.name;  // in case it gets ellipsized
 
-    const sensorInfo = card.querySelector('.sensor-info');
-    sensorInfo.textContent = `
-        created on ${sensor.created_on};
-        ${todayReadingsCount} readings today
-    `;
+    const createdOnValue = card.querySelector('.created-on');
+    createdOnValue.textContent = formatDateToCompactDatetime(
+        new Date(sensor.created_on), locales
+    );
+    const readingsTodayValue = card.querySelector('.readings-today');
+    readingsTodayValue.textContent = todayReadingsCount;
 
     return card;
 }
@@ -28,10 +37,12 @@ document.addEventListener('DOMContentLoaded', async () => {
     const todayCountsResponse = await Api.fetchTodayReadingsCounts();
     const todayCounts = await todayCountsResponse.json();
 
+    const locales = getUserLocales();
+
     const fragment = new DocumentFragment();
     for (const sensor of sensors) {
         const sensorId = sensor.id;
-        const sensorCard = createSensorCard(sensor, todayCounts[sensorId]);
+        const sensorCard = createSensorCard(sensor, todayCounts[sensorId], locales);
         fragment.append(sensorCard);
     }
 
