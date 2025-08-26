586b2974e7eecaa62573dfa1afaa89b34bf36140
purewave0 <purewave_@outlook.com>
2025-08-26 19:59:05 -0300

    refactor(main): merge preferences cache + updater into preferences-cache.js

--- app/static/js/preferences-cache.js
+++ app/static/js/preferences-cache.js
@@ -53,3 +53,28 @@ class PreferencesCache {
         return localStorage.setItem(PreferencesCache.#KEYS.lastUpdateTime, time);
     }
 };
+
+
+const PreferencesCacheUpdater = {
+    async updateIfNeeded() {
+        const response = await Api.fetchPreferencesLastUpdateTime();
+        const lastUpdateTime = await response.json();
+        if (PreferencesCache.getLastUpdateTime() !== lastUpdateTime) {
+            console.log(
+                '[preferences.js] stored preferences nonexistent or outdated.'
+            );
+            PreferencesCache.setLastUpdateTime(lastUpdateTime);
+            const preferencesResponse = await Api.fetchPreferences();
+            const preferences = await preferencesResponse.json();
+            PreferencesCache.set(
+                preferences.display_name,
+                preferences.username,
+                preferences.homepage,
+                preferences.temperature_unit
+            );
+            console.log('[preferences.js] preferences updated.');
+        }
+    }
+}
+
+PreferencesCacheUpdater.updateIfNeeded();
