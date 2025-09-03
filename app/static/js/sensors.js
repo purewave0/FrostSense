bdd4a266cda31248a729a0b8fa97bb37b72b06cd
purewave0 <purewave_@outlook.com>
2025-09-03 11:32:58 -0300

    feat(main): add Delete Sensor modal

--- app/static/js/sensors.js
+++ app/static/js/sensors.js
@@ -1,6 +1,9 @@
 function createSensorCard(sensor, todayReadingsCount, locales) {
     const card = document.createElement('div');
     card.className = 'sensor';
+    card.dataset.id = sensor.id;
+    card.dataset.name = sensor.name;
+    card.dataset.createdOn = sensor.created_on;
     card.innerHTML = `
         <div class="sensor-header">
             <div class="sensor-name-wrapper">
@@ -56,7 +59,18 @@ document.addEventListener('DOMContentLoaded', async () => {
 
     const optionsMenu = document.getElementById('sensor-options-menu');
 
+    let currentlySelectedSensorCard = null;
+
+    function areAnyModalsOpen() {
+        return document.querySelector('.modal.is-open') !== null;
+    }
+
     function menuOutsideClickHandler(event) {
+        if (areAnyModalsOpen()) {
+            // while modals are open, we must keep the menu open too so the sensor
+            // remains 'selected'
+            return;
+        }
         if (!optionsMenu.contains(event.target)) {
             const sensorCurrentlyShowingOptions = sensorsDestination
                 .querySelector('.showing-options');
@@ -67,18 +81,19 @@ document.addEventListener('DOMContentLoaded', async () => {
     }
 
     function menuEscHandler(event) {
+        if (areAnyModalsOpen()) {
+            return;
+        }
         if (event.key === 'Escape') {
-            const sensorCurrentlyShowingOptions = sensorsDestination
-                .querySelector('.showing-options');
             const menuButton =
-                sensorCurrentlyShowingOptions.querySelector('.sensor-options-button');
+                currentlySelectedSensorCard.querySelector('.sensor-options-button');
             menuButton.dispatchEvent(new Event('click'));
         }
     }
 
     function showMenu(sensorCard) {
-        const optionsButton = sensorCard.querySelector('.sensor-options-button');
-        FloatingUIDOM.computePosition(optionsButton, optionsMenu, {
+        const menuButton = sensorCard.querySelector('.sensor-options-button');
+        FloatingUIDOM.computePosition(menuButton, optionsMenu, {
             placement: 'right-start',
             middleware: [
                 FloatingUIDOM.offset(4),
@@ -94,14 +109,14 @@ document.addEventListener('DOMContentLoaded', async () => {
             });
             document.body.addEventListener('click', menuOutsideClickHandler);
             document.body.addEventListener('keydown', menuEscHandler);
+            currentlySelectedSensorCard = sensorCard;
         });
     }
 
     function hideMenu() {
-        const sensorCurrentlyShowingOptions = sensorsDestination
-            .querySelector('.showing-options');
-        if (sensorCurrentlyShowingOptions) {
-            sensorCurrentlyShowingOptions.classList.remove('showing-options');
+        if (currentlySelectedSensorCard) {
+            currentlySelectedSensorCard.classList.remove('showing-options');
+            currentlySelectedSensorCard = null;
         }
         optionsMenu.classList.remove('visible');
         document.body.removeEventListener('click', menuOutsideClickHandler);
@@ -120,7 +135,8 @@ document.addEventListener('DOMContentLoaded', async () => {
                 sensorCard.classList.contains('showing-options');
             hideMenu();
             if (!isThisShowingOptions) {
-                // another card is currently showing options. change to this one
+                // another card is currently showing its menu. show this one's
+                // instead
                 showMenu(sensorCard);
             }
 
@@ -135,4 +151,41 @@ document.addEventListener('DOMContentLoaded', async () => {
     createButton.disabled = false;
 
     createButton.addEventListener('click', () => { alert('TODO') });
+
+    const editOption = document.getElementById('option-edit');
+    editOption.addEventListener('click', () => {
+        alert('edit TODO');
+    });
+
+
+    const manageKeyOption = document.getElementById('option-manage-key');
+    manageKeyOption.addEventListener('click', () => {
+        alert('manageKey TODO');
+    });
+
+    const deleteModal = {
+        'name': document.getElementById('modal-delete-name'),
+        'createdOn': document.getElementById('modal-delete-created-on'),
+        'deleteButton': document.getElementById('modal-delete-delete'),
+    };
+    deleteModal.deleteButton.addEventListener('click', () => {
+        alert('TODO deleting sensor with id ' + currentlySelectedSensorCard.dataset.id);
+        MicroModal.close('modal-delete');
+        hideMenu();
+    });
+
+    function openDeleteModal(name, createdOn) {
+        deleteModal.name.textContent = name;
+        deleteModal.createdOn.textContent = formatDateToCompactDatetime(
+            new Date(createdOn), locales
+        );
+        MicroModal.show('modal-delete');
+    }
+
+    const deleteOption = document.getElementById('option-delete');
+    deleteOption.addEventListener('click', () => {
+        const name = currentlySelectedSensorCard.dataset.name;
+        const createdOn = new Date(currentlySelectedSensorCard.dataset.createdOn);
+        openDeleteModal(name, createdOn);
+    });
 });
