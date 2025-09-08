ab6bcd23c7b12b46301c2381b0dcc7fc318f9dbd
purewave0 <purewave_@outlook.com>
2025-09-08 17:41:03 -0300

    feat(main): display Permissions on cell hover in the Users table

--- app/static/js/users.js
+++ app/static/js/users.js
@@ -1,3 +1,61 @@
+const allPermissions = [
+    {
+        value: 1,
+        name: 'Manage reports',
+    },
+    {
+        value: 2,
+        name: 'Edit sensors',
+    },
+    {
+        value: 4,
+        name: 'Manage users',
+    },
+    {
+        value: 8,
+        name: 'Manage system settings',
+    },
+];
+
+/**
+ * Return true if `permissionsValue` includes `permission`.
+ *
+ * @param {number} permissionsValue The current permissions.
+ * @param {number} permission The permission to check for.
+ */
+function containsPermission(permissionsValue, permission) {
+    return (permissionsValue & permission) !== 0;
+}
+
+
+/**
+ * Return an array of each permission included in the given permissions value.
+ *
+ * @param {number} permissionsValue The current permissions.
+ */
+function extractPermissionsFromNumber(permissionsValue) {
+    return allPermissions.filter((permission) => {
+        return containsPermission(permissionsValue, permission.value)
+    });
+}
+
+
+/**
+ * Return a string with every given permission's name, one per line.
+ *
+ * Each line starts with a hyphen (-). If there are no permissions at all,
+ * "No permissions" is returned.
+ *
+ * @param {number} permissionsValue The current permissions.
+ */
+function formatPermissionsValue(permissionsValue) {
+    if (permissionsValue === 0) {
+        return 'No permissions.'
+    }
+    return '- ' + extractPermissionsFromNumber(permissionsValue)
+        .map((permission) => permission.name)
+        .join('\n- ');
+}
 
 document.addEventListener('DOMContentLoaded', async () => {
     const locales = getUserLocales();
@@ -25,6 +83,17 @@ document.addEventListener('DOMContentLoaded', async () => {
                 data: 'permissions',
                 searchable: false,
                 orderable: false,
+                type: 'string',  /* don't treat it as a number */
+                width: '160px',
+                render: (data, type) => {
+                    if (type === 'display') {
+                        return '<span class="permissions-cell">…</span>';
+                    }
+                    return data;
+                },
+                createdCell: (cell, data) => {
+                    cell.title = formatPermissionsValue(data);
+                }
             },
             {
                 name: 'updated_on',
@@ -61,7 +130,7 @@ document.addEventListener('DOMContentLoaded', async () => {
                 'id': user.id,
                 'display_name': user.display_name,
                 'username': user.username,
-                'permissions': 'TODO', // TODO
+                'permissions': user.permissions,
                 'updated_on': user.updated_on,
                 'created_on': user.created_on,
             }
