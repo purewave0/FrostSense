8026d9be57d32832716394b68087e610dcf43a1c
purewave0 <purewave_@outlook.com>
2025-09-19 18:39:18 -0300

    feat(main/users): add Delete User button

--- app/static/js/users.js
+++ app/static/js/users.js
@@ -178,9 +178,33 @@ document.addEventListener('DOMContentLoaded', async () => {
                         openResetPasswordModal(user.display_name);
                     });
 
+                    const deleteButton = document.createElement('button');
+                    deleteButton.className = 'action action-delete';
+                    deleteButton.title = 'Delete';
+                    deleteButton.innerHTML = `
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            height="24px"
+                            viewBox="0 -960 960 960"
+                            width="24px"
+                            fill="#e3e3e3"
+                        >
+                            <path d="M280-120q-33 0-56.5-23.5T200-200v-520h-40v-80h200v-40h240v40h200v80h-40v520q0 33-23.5 56.5T680-120H280Zm80-160h80v-360h-80v360Zm160 0h80v-360h-80v360Z"/>
+                        </svg>
+                    `;
+                    deleteButton.addEventListener('click', () => {
+                        selectedUser = user;
+                        openDeleteModal(
+                            user.display_name,
+                            user.username,
+                            new Date(user.updated_on),
+                            new Date(user.created_on)
+                        );
+                    });
+
                     const cell = document.createElement('div');
                     cell.className = 'actions-cell';
-                    cell.append(editButton, resetPasswordButton);
+                    cell.append(editButton, resetPasswordButton, deleteButton);
                     return cell;
                 }
             }
@@ -507,5 +531,27 @@ document.addEventListener('DOMContentLoaded', async () => {
     function openResetPasswordModal(displayName) {
         resetPasswordModal.displayName.textContent = displayName;
         MicroModal.show('modal-reset-password');
+    }
+
+
+    // -- Delete modal --
+    const deleteModal = {
+        'displayName': document.getElementById('modal-delete-display-name'),
+        'username': document.getElementById('modal-delete-username'),
+        'created_on': document.getElementById('modal-delete-created-on'),
+        'updated_on': document.getElementById('modal-delete-updated-on'),
+        'confirmButton': document.getElementById('modal-delete-confirm'),
+    };
+    deleteModal.confirmButton.addEventListener('click', async () => {
+        alert('TODO')
+        MicroModal.close('modal-delete');
+    });
+
+    function openDeleteModal(displayName, username, updatedOn, createdOn) {
+        deleteModal.displayName.textContent = displayName;
+        deleteModal.username.textContent = username;
+        deleteModal.updated_on.textContent = formatDateToCompactDatetime(updatedOn);
+        deleteModal.created_on.textContent = formatDateToCompactDatetime(createdOn);
+        MicroModal.show('modal-delete');
     }
 });
