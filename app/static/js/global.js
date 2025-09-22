69dde7a3aa0609fdb0a14ebd109422887634e09d
purewave0 <purewave_@outlook.com>
2025-09-22 14:32:41 -0300

    feat(js): add toast functionality

--- app/static/js/global.js
+++ app/static/js/global.js
@@ -260,6 +260,58 @@ function getStartOfToday() {
     return date;
 }
 
+const ToastType = {
+    'SUCCESS': {
+        'backgroundColor': 'var(--color-toast-success)',
+        'icon': `
+            <svg
+                xmlns="http://www.w3.org/2000/svg"
+                height="24px"
+                viewBox="0 -960 960 960"
+                width="24px"
+                fill="#e3e3e3"
+            >
+                <path d="M382-240 154-468l57-57 171 171 367-367 57 57-424 424Z"/>
+            </svg>
+        `,
+    },
+    'ERROR': {
+        'backgroundColor': 'var(--color-toast-error)',
+        'icon': `
+            <svg
+                xmlns="http://www.w3.org/2000/svg"
+                height="24px"
+                viewBox="0 -960 960 960"
+                width="24px"
+                fill="#e3e3e3"
+            >
+                <path d="M440-280h80v-240h-80v240Zm40-320q17 0 28.5-11.5T520-640q0-17-11.5-28.5T480-680q-17 0-28.5 11.5T440-640q0 17 11.5 28.5T480-600Zm0 520q-83 0-156-31.5T197-197q-54-54-85.5-127T80-480q0-83 31.5-156T197-763q54-54 127-85.5T480-880q83 0 156 31.5T763-763q54 54 85.5 127T880-480q0 83-31.5 156T763-197q-54 54-127 85.5T480-80Z"/>
+            </svg>
+        `,
+    },
+};
+
+
+/**
+ * Show a toast of the given type with the given text for 3 seconds.
+ *
+ * @param {object} type The toast type, according to `ToastType`.
+ * @param {string} text The toast's contents.
+ */
+function showToast(type, text) {
+    Toastify({
+        text: text,
+        duration: 3_000,
+        close: true,
+        stopOnFocus: false,
+        gravity: 'bottom',
+        position: 'right',
+        style: {
+            background: type.backgroundColor,
+        },
+    }).showToast();
+}
+
 
 /** Interval (in milliseconds) at which gauges and graphs will fetch updates. */
 const UPDATE_INTERVAL = 2_000;  // TODO: proper value. 2s is just for testing
@@ -268,5 +320,4 @@ const ReadingsReport = {
     /** How many rows one table has. Any more, and the next readings go into a new
      *  table. */
     ROWS_PER_TABLE: 80,
-
 }
