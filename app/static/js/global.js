e728c92eef468282cca0195764c1f05470c1908f
purewave0 <purewave_@outlook.com>
2025-09-22 20:46:22 -0300

    feat(js): add icons to toasts

--- app/static/js/global.js
+++ app/static/js/global.js
@@ -263,31 +263,11 @@ function getStartOfToday() {
 const ToastType = {
     'SUCCESS': {
         'backgroundColor': 'var(--color-toast-success)',
-        'icon': `
-            <svg
-                xmlns="http://www.w3.org/2000/svg"
-                height="24px"
-                viewBox="0 -960 960 960"
-                width="24px"
-                fill="#e3e3e3"
-            >
-                <path d="M382-240 154-468l57-57 171 171 367-367 57 57-424 424Z"/>
-            </svg>
-        `,
+        'iconURL': '/static/icon/success.svg',
     },
     'ERROR': {
         'backgroundColor': 'var(--color-toast-error)',
-        'icon': `
-            <svg
-                xmlns="http://www.w3.org/2000/svg"
-                height="24px"
-                viewBox="0 -960 960 960"
-                width="24px"
-                fill="#e3e3e3"
-            >
-                <path d="M440-280h80v-240h-80v240Zm40-320q17 0 28.5-11.5T520-640q0-17-11.5-28.5T480-680q-17 0-28.5 11.5T440-640q0 17 11.5 28.5T480-600Zm0 520q-83 0-156-31.5T197-197q-54-54-85.5-127T80-480q0-83 31.5-156T197-763q54-54 127-85.5T480-880q83 0 156 31.5T763-763q54 54 85.5 127T880-480q0 83-31.5 156T763-197q-54 54-127 85.5T480-80Z"/>
-            </svg>
-        `,
+        'iconURL': '/static/icon/error.svg',
     },
 };
 
@@ -306,6 +286,7 @@ function showToast(type, text) {
         stopOnFocus: false,
         gravity: 'bottom',
         position: 'right',
+        avatar: type.iconURL,
         style: {
             background: type.backgroundColor,
         },
