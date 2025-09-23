269c86e5822232ff7a9ff8bc55aac8c53c27b943
purewave0 <purewave_@outlook.com>
2025-09-23 16:33:18 -0300

    feat(main): require login + permanent password for relevant routes

--- app/main/routes.py
+++ app/main/routes.py
@@ -4,7 +4,7 @@ from flask_login import current_user, login_required, logout_user
 from app.main import bp
 from app.api.report import get_report_file
 from app.models.users import User
-from app.util import permission_required
+from app.util import permission_required, login_and_permanent_password_required
 
 
 @bp.route('/login')
@@ -16,34 +16,34 @@ def login():
 
 
 @bp.route('/')
-@login_required
+@login_and_permanent_password_required
 def index():
     return redirect('/' + current_user.homepage)
 
 
 @bp.route('/readings')
-@login_required
+@login_and_permanent_password_required
 def readings():
     return render_template('main/sections/readings.html')
 
 @bp.route('/history')
-@login_required
+@login_and_permanent_password_required
 def history():
     return render_template('main/sections/history.html')
 
 @bp.route('/sensors')
-@login_required
+@login_and_permanent_password_required
 def sensors():
     return render_template('main/sections/sensors.html')
 
 @bp.route('/reports')
-@login_required
+@login_and_permanent_password_required
 @permission_required(User.Permission.MANAGE_REPORTS)
 def reports():
     return render_template('main/sections/reports.html')
 
 @bp.route('/reports/<code>')
-@login_required
+@login_and_permanent_password_required
 @permission_required(User.Permission.MANAGE_REPORTS)
 def get_report(code):
     try:
@@ -54,13 +54,13 @@ def get_report(code):
     return report_html
 
 @bp.route('/verify-reports')
-@login_required
+@login_and_permanent_password_required
 @permission_required(User.Permission.MANAGE_REPORTS)
 def verify_reports():
     return render_template('main/sections/verify-reports.html')
 
 @bp.route('/preferences')
-@login_required
+@login_and_permanent_password_required
 def own_preferences():
     return render_template('main/preferences.html')
 
@@ -71,13 +71,13 @@ def main_logout():
     return redirect(url_for('main.login'))
 
 @bp.route('/users')
-@login_required
+@login_and_permanent_password_required
 @permission_required(User.Permission.MANAGE_USERS)
 def users():
     return render_template('main/sections/users.html')
 
 @bp.route('/system-settings')
-@login_required
+@login_and_permanent_password_required
 @permission_required(User.Permission.MANAGE_SYSTEM_SETTINGS)
 def system_settings():
     return render_template('main/system-settings.html')
