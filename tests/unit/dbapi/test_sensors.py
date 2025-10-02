46df184156173877fc136d47af2cc4fa6c162587
purewave0 <purewave_@outlook.com>
2025-10-02 15:36:40 -0300

    refactor(tests/test_sensors): use the sensor fixture

--- tests/unit/dbapi/test_sensors.py
+++ tests/unit/dbapi/test_sensors.py
@@ -32,12 +32,8 @@ def test_create_sensor_result_and_persistence(app):
 
 # delete
 
-def test_delete_sensor_by_id_persistence(app):
-    sensor = Sensor('Test sensor')
+def test_delete_sensor_by_id_persistence(app, sensor):
     with app.app_context():
-        db.session.add(sensor)
-        db.session.commit()
-
         delete_sensor_by_id(sensor.id)
 
         stored_sensor = db.session.execute(
@@ -50,35 +46,31 @@ def test_delete_sensor_by_id_persistence(app):
         assert stored_sensor is None
 
 
-# retrieve
+# get
 
 def test_get_sensors_order_and_result(app):
     with app.app_context():
-        added_sensors = [
+        sensors = [
             Sensor('Test sensor 1'),
             Sensor('Test sensor 2'),
             Sensor('Test sensor 3'),
         ]
-        for sensor in added_sensors:
+        for sensor in sensors:
             db.session.add(sensor)
         db.session.commit()
 
         returned_sensors = get_sensors()
-        assert len(returned_sensors) == len(added_sensors)
+        assert len(returned_sensors) == len(sensors)
 
-        for returned_sensor, added_sensor in zip(
-            returned_sensors, added_sensors
+        for returned_sensor, sensor in zip(
+            returned_sensors, sensors
         ):
-            assert returned_sensor['id'] == added_sensor.id
-            assert returned_sensor['name'] == added_sensor.name
-            assert returned_sensor['created_on'] == added_sensor.created_on
+            assert returned_sensor['id'] == sensor.id
+            assert returned_sensor['name'] == sensor.name
+            assert returned_sensor['created_on'] == sensor.created_on
 
-def test_get_sensor_by_id_found_and_not_found(app):
-    sensor = Sensor('Test sensor')
+def test_get_sensor_by_id_found_and_not_found(app, sensor):
     with app.app_context():
-        db.session.add(sensor)
-        db.session.commit()
-
         found = get_sensor_by_id(sensor.id)
         assert found is not None
         assert found.id == sensor.id
@@ -87,32 +79,28 @@ def test_get_sensor_by_id_found_and_not_found(app):
         assert get_sensor_by_id(12345) is None
 
 
-# retrieve attribute
+# get attribute
 
 def test_get_sensor_ids_order_and_result(app):
-    added_sensors: list[Sensor] = []
-    names = ['Test sensor 1', 'Test sensor 2', 'Test sensor 3']
+    sensors: list[Sensor] = []
+    names = ('Test sensor 1', 'Test sensor 2', 'Test sensor 3')
     with app.app_context():
         for name in names:
             sensor = Sensor(name)
             db.session.add(sensor)
-            added_sensors.append(sensor)
+            sensors.append(sensor)
         db.session.commit()
 
         returned_sensor_ids = get_sensor_ids()
-        assert len(returned_sensor_ids) == len(added_sensors)
+        assert len(returned_sensor_ids) == len(sensors)
 
-        for returned_sensor_id, added_sensor in zip(
-            returned_sensor_ids, added_sensors
+        for returned_sensor_id, sensor in zip(
+            returned_sensor_ids, sensors
         ):
-            assert returned_sensor_id == added_sensor.id
+            assert returned_sensor_id == sensor.id
 
-def test_get_sensor_key_by_id_found_and_not_found(app):
-    sensor = Sensor('Test sensor')
+def test_get_sensor_key_by_id_found_and_not_found(app, sensor):
     with app.app_context():
-        db.session.add(sensor)
-        db.session.commit()
-
         found = get_sensor_key_by_id(sensor.id)
         assert found == sensor.key
 
@@ -121,47 +109,46 @@ def test_get_sensor_key_by_id_found_and_not_found(app):
 
 # exists
 
-def test_sensor_id_exists_found_and_not_found(app):
-    sensor = Sensor('Test sensor')
+def test_sensor_id_exists_found_and_not_found(app, sensor):
     with app.app_context():
-        db.session.add(sensor)
-        db.session.commit()
-
         assert sensor_id_exists(sensor.id)
 
         assert not sensor_id_exists(12345)
 
-def test_sensor_name_exists_found_and_not_found(app):
-    sensor = Sensor('Test sensor')
+def test_sensor_name_exists_found_and_not_found(app, sensor):
     with app.app_context():
-        db.session.add(sensor)
-        db.session.commit()
-
-        assert sensor_name_exists('Test sensor')
+        assert sensor_name_exists(sensor.name)
 
         assert not sensor_name_exists('Unknown sensor')
 
 
 # update
 
-def test_reset_sensor_key_by_id_result_and_difference(app):
-    sensor = Sensor('Test sensor')
+def test_reset_sensor_key_by_id_result_and_difference(app, sensor):
     with app.app_context():
-        db.session.add(sensor)
-        db.session.commit()
         old_key = sensor.key
 
         new_key = reset_sensor_key_by_id(sensor.id)
-        assert new_key == sensor.key
+        updated_sensor = db.session.execute(
+            db.select(
+                Sensor
+            ).where(
+                Sensor.id == sensor.id
+            )
+        ).scalar_one()
+        assert new_key == updated_sensor.key
 
-        assert old_key != sensor.key
+        assert old_key != updated_sensor.key
 
-def test_update_sensor_by_id_persistence(app):
-    sensor = Sensor('Test sensor')
+def test_update_sensor_by_id_persistence(app, sensor):
     with app.app_context():
-        db.session.add(sensor)
-        db.session.commit()
-
         update_sensor_by_id(sensor.id, 'Updated test sensor')
 
-        assert sensor.name == 'Updated test sensor'
+        updated_sensor = db.session.execute(
+            db.select(
+                Sensor
+            ).where(
+                Sensor.id == sensor.id
+            )
+        ).scalar_one()
+        assert updated_sensor.name == 'Updated test sensor'
