3f0913b1a422ddf6c944e74615cfaadf88948687
purewave0 <purewave_@outlook.com>
2025-10-02 19:58:01 -0300

    test(conftest): add sensors fixture

--- tests/conftest.py
+++ tests/conftest.py
@@ -25,9 +25,26 @@ def runner(app):
 
 @pytest.fixture()
 def sensor(app):
-    """Create a sensor called 'Test sensor' and return it."""
+    """Create a sensor named 'Test sensor' and return it."""
     with app.app_context():
         created_sensor = Sensor('Test sensor')
         db.session.add(created_sensor)
         db.session.commit()
         yield created_sensor
+
+
+@pytest.fixture()
+def sensors(app):
+    """Create three sensors named 'Test sensor 1', 'Test sensor 2', and
+    'Test sensor 3', respectively, and return them.
+    """
+    with app.app_context():
+        created_sensors = (
+            Sensor('Test sensor 1'),
+            Sensor('Test sensor 2'),
+            Sensor('Test sensor 3'),
+        )
+        for sensor in created_sensors:
+            db.session.add(sensor)
+        db.session.commit()
+        yield created_sensors
